dadc457ac49ec0518ac527096e16725699062be1
Bruno Menendes Rodrigues <menendesbruno@gmail.com>
2020-12-14 12:12:04 -0300

    Continuando

--- CursoMVC/CursoMVC/Models/Contexto.cs
+++ CursoMVC/CursoMVC/Models/Contexto.cs
@@ -9,6 +9,7 @@ namespace CursoMVC.Models
     public class Contexto : DbContext
     {
         public DbSet<Categoria> categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\msqllocaldb;Database = CursoMV; Integrated Security=True");
